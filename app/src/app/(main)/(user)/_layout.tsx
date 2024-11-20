698d5a28029ec81059a12a43696572b6cebf0a84
nobrelli <ryuedwards@gmail.com>
2024-11-20 10:26:52 +0800

    Removed duplicate loading of fonts

--- app/src/app/(main)/(user)/_layout.tsx
+++ app/src/app/(main)/(user)/_layout.tsx
@@ -1,10 +1,6 @@
 import { CustomStack } from '@/components/navigation/CustomStack'
-import { SkinnedSpinner } from '@/components/skinned/SkinnedSpinner'
 import { useRenderCount } from '@/hooks/useRenderCount'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
-import { useFonts } from 'expo-font'
-import { SplashScreen, Stack } from 'expo-router'
-import { useEffect } from 'react'
+import { Stack } from 'expo-router'
 
 export const unstable_settings = {
   initialRouteName: '(tabs)',
@@ -12,35 +8,6 @@ export const unstable_settings = {
 
 export default function MainLayout() {
   useRenderCount('MainLayout')
-
-  const [fontsLoaded, fontsError] = useFonts({
-    thin: require('@/assets/fonts/montserrat/montserrat-latin-100-normal.ttf'),
-    extraLight: require('@/assets/fonts/montserrat/montserrat-latin-200-normal.ttf'),
-    light: require('@/assets/fonts/montserrat/montserrat-latin-300-normal.ttf'),
-    regular: require('@/assets/fonts/montserrat/montserrat-latin-400-normal.ttf'),
-    medium: require('@/assets/fonts/montserrat/montserrat-latin-500-normal.ttf'),
-    semiBold: require('@/assets/fonts/montserrat/montserrat-latin-600-normal.ttf'),
-    bold: require('@/assets/fonts/montserrat/montserrat-latin-700-normal.ttf'),
-    extraBold: require('@/assets/fonts/montserrat/montserrat-latin-800-normal.ttf'),
-    black: require('@/assets/fonts/montserrat/montserrat-latin-900-normal.ttf'),
-    ...MaterialCommunityIcons.font,
-  })
-
-  useEffect(() => {
-    if (fontsError) throw fontsError
-  }, [fontsError])
-
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync().then(() => {
-        console.info('App loaded')
-      })
-    }
-  }, [fontsLoaded])
-
-  if (!fontsLoaded) {
-    return <SkinnedSpinner />
-  }
 
   return (
     <CustomStack>
