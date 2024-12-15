b4671ff06da380769f774657ad37728063919f59
nobrelli <ryuedwards@gmail.com>
2024-12-15 11:21:02 +0800

    Added create job fields

--- app/src/types/Fields.ts
+++ app/src/types/Fields.ts
@@ -1,9 +1,5 @@
 import type { Role } from './Enums'
 
-export interface CreateJobFields {
-  jobTitle: string
-}
-
 export interface SignInFields {
   email: string
   phone: string
@@ -23,4 +19,20 @@ export interface SignUpFields extends SignInFields {
   role: Role
   firstName: string
   lastName: string
+}
+
+export interface CreateJobFields {
+  title: string
+  category: string
+  employmentType: string
+  location: string
+  summary: string
+  description: string
+  postAs: string
+  deadline: string
+  startDate: string
+  endDate: string
+  pay: string
+  payPeriod: string
+  isPayRange: string
 }
