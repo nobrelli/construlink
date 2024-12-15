8fecfb1c7aec5aaaa33e0011c53ebda11666f2eb
nobrelli <ryuedwards@gmail.com>
2024-12-15 11:20:31 +0800

    Added schemas types

---
+++ app/src/types/Schemas.ts
@@ -0,0 +1,34 @@
+import type { FirebaseFirestoreTypes } from '@react-native-firebase/firestore'
+import type { Role } from './Enums'
+
+export interface ErrorResponse {
+  message?: string
+}
+
+type HasKey = { key: string }
+
+export interface UserSchema extends HasKey {
+  firstName: string
+  lastName: string
+  role: Role
+}
+
+export interface TradespersonSchema extends UserSchema {
+  expertise: string[]
+}
+
+export interface JobSchema extends HasKey {
+  authorId: string
+  title: string
+  category: string
+  description: string
+  location: string
+  employmentType: string
+  deadline?: FirebaseFirestoreTypes.Timestamp
+  hoursType?: string
+  start?: FirebaseFirestoreTypes.Timestamp
+  end?: FirebaseFirestoreTypes.Timestamp
+  payType?: string
+  payAmount?: number
+  postAs: string
+}
