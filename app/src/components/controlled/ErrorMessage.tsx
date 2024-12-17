5a156e16ba750463a427541b5be72961f88bceef
nobrelli <ryuedwards@gmail.com>
2024-12-17 23:45:05 +0800

    Modified ErrorMessage component

--- app/src/components/controlled/ErrorMessage.tsx
+++ app/src/components/controlled/ErrorMessage.tsx
@@ -1,25 +1,33 @@
 import { resolveColor } from '@/helpers/resolveColor'
 import { useThemeStore } from '@/stores/theme'
 import { Typo } from '@/theme'
-import type { TextProps } from 'react-native'
+import { type TextProps, View } from 'react-native'
 import { ClText } from '../ClText'
 
 export default ({ message, ...rest }: TextProps & { message?: string }) => {
   const colors = useThemeStore((state) => state.colors)
 
   return (
-    <ClText
-      type="helper"
+    <View
       style={{
-        color: resolveColor(
-          colors.states.danger[300],
-          colors.states.danger[400]
-        ),
-        fontFamily: Typo.family.semiBold,
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
       }}
-      {...rest}
     >
-      {message}
-    </ClText>
+      <ClText
+        type="helper"
+        style={{
+          color: resolveColor(
+            colors.states.danger[300],
+            colors.states.danger[400]
+          ),
+          fontFamily: Typo.family.semiBold,
+        }}
+        {...rest}
+      >
+        {message}
+      </ClText>
+    </View>
   )
 }
