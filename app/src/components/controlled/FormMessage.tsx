45af4ae9393138173ffbed064dffd3a715ad9e2b
nobrelli <ryuedwards@gmail.com>
2024-12-22 22:57:37 +0800

    Modified FormMessage component

--- app/src/components/controlled/FormMessage.tsx
+++ app/src/components/controlled/FormMessage.tsx
@@ -1,17 +1,18 @@
 import { createStyles } from '@/helpers/createStyles'
-import { Sizes } from '@/theme'
+import { resolveColor } from '@/helpers/resolveColor'
+import { Sizes, Spacing } from '@/theme'
 import type { UIState } from '@/theme/palette'
 import { type GlyphMap, IconSet } from '@/types/Icons'
 import { View } from 'react-native'
 import { ClIcon } from '../ClIcon'
 import { ClText } from '../ClText'
 
-interface IFormMessageProps {
+interface FormMessageProps {
   message: string
   state: UIState
 }
 
-export default ({ message, state }: IFormMessageProps) => {
+export function FormMessage({ message, state }: FormMessageProps) {
   const styles = useStyles({ state })
   let iconName: GlyphMap['MaterialIcons']
 
@@ -32,7 +33,14 @@ export default ({ message, state }: IFormMessageProps) => {
 
   return (
     <View style={styles.container}>
-      <ClIcon set={IconSet.MaterialIcon} name={iconName} size={Sizes.icon.md} />
+      <View style={{ marginTop: Spacing[1] }}>
+        <ClIcon
+          set={IconSet.MaterialIcon}
+          name={iconName}
+          size={styles.icon.fontSize}
+          color={styles.icon.color}
+        />
+      </View>
       <View style={styles.textWrapper}>
         <ClText style={styles.message}>{message}</ClText>
       </View>
@@ -41,14 +49,26 @@ export default ({ message, state }: IFormMessageProps) => {
 }
 
 const useStyles = createStyles(
-  ({ spacing, sizes, colors }, { state }: { state: UIState }) => ({
+  ({ spacing, sizes, colors, typo }, { state }: { state: UIState }) => ({
     container: {
       flex: 1,
       flexDirection: 'row',
       gap: spacing[2],
       padding: spacing[4],
       borderRadius: sizes.radius['2xl'],
-      backgroundColor: colors.states[state].base,
+      backgroundColor: resolveColor(
+        colors.states[state][900],
+        colors.states[state][50]
+      ),
+      borderWidth: sizes.borderWidth.thin,
+      borderColor: resolveColor(
+        colors.states[state][500],
+        colors.states[state][400]
+      ),
+    },
+    icon: {
+      fontSize: sizes.icon.md,
+      color: resolveColor(colors.states[state].base, colors.states[state][700]),
     },
     textWrapper: {
       flex: 1,
@@ -56,7 +76,8 @@ const useStyles = createStyles(
       flexWrap: 'wrap',
     },
     message: {
-      color: colors.white,
+      color: colors.primaryText,
+      fontSize: typo.presets.sm.fontSize,
     },
   })
 )
