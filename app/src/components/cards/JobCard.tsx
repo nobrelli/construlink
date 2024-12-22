9a69e781de9052ae4e08e3fe8b95f3233456de44
nobrelli <ryuedwards@gmail.com>
2024-12-22 13:17:24 +0800

    Modified JobCard component

--- app/src/components/cards/JobCard.tsx
+++ app/src/components/cards/JobCard.tsx
@@ -1,30 +1,40 @@
-import { createStyles } from '@/helpers/createStyles'
-import { resolveColor } from '@/helpers/resolveColor'
-import { capitalizeFirst } from '@/helpers/stringUtils'
 import { Cl } from '@/lib/options'
+import { getCompanyDetails } from '@/services/company'
 import typography from '@/theme/typography'
 import { IconSet } from '@/types/Icons'
+import type { JobSchema } from '@/types/Schemas'
+import { useMount } from '@reactuses/core'
+import { formatDistanceToNowStrict } from 'date-fns'
+import { router, useFocusEffect } from 'expo-router'
+import { useEffect, useState } from 'react'
 import { View } from 'react-native'
+import { ClCard } from '../ClCard'
 import { ClIconText } from '../ClIconText'
 import { ClText } from '../ClText'
 
-interface JobCardProps {
-  title: string
-  author: string
-  location: string
-  description: string
-  duration: string
-  employmentType: string
-  pay?: string
+interface JobCardProps
+  extends Pick<
+    JobSchema,
+    'authorId' | 'title' | 'location' | 'postAs' | 'employmentType'
+  > {
+  jobId: string
+  postTime: Date
 }
 
 export function JobCard(props: JobCardProps) {
-  const { title, author, location, description, duration, employmentType } =
-    props
-  const styles = useStyles()
+  const { jobId, title, location, postAs, postTime, employmentType } = props
+
+  const handleViewJobPost = () => {
+    router.navigate({
+      pathname: '/job/[jobId]',
+      params: {
+        jobId,
+      },
+    })
+  }
 
   return (
-    <View style={styles.card}>
+    <ClCard onPress={handleViewJobPost}>
       <View>
         <ClText
           weight="bold"
@@ -32,9 +42,7 @@ export function JobCard(props: JobCardProps) {
         >
           {title}
         </ClText>
-        <ClText style={{ fontSize: typography.presets.base.fontSize }}>
-          {author}
-        </ClText>
+        <JobAuthor authorId={props.authorId} postAs={postAs} />
       </View>
       <View>
         <ClIconText
@@ -50,33 +58,71 @@ export function JobCard(props: JobCardProps) {
             set: IconSet.MaterialCommunityIcons,
             name: 'briefcase',
           }}
-          // biome-ignore lint/style/noNonNullAssertion:
           text={
+            // biome-ignore lint/style/noNonNullAssertion: <explanation>
             Cl.employmentTypes.find((option) => option.value === employmentType)
               ?.label!
           }
           style={{ fontSize: typography.presets.sm.fontSize }}
         />
       </View>
-      <ClText
-        style={{ fontSize: typography.presets.xs.fontSize }}
-        type="helper"
-        dim
-      >
-        Posted {duration} ago
-      </ClText>
-    </View>
+      <JobPostedAgo timestamp={postTime} />
+    </ClCard>
   )
 }
 
-const useStyles = createStyles(({ colors, sizes, spacing, typo }) => ({
-  card: {
-    backgroundColor: resolveColor(colors.neutral[800], colors.neutral[100]),
-    borderRadius: sizes.radius['2xl'],
-    borderWidth: sizes.borderWidth.thin,
-    borderColor: resolveColor(colors.neutral[700], colors.neutral[200]),
-    paddingHorizontal: spacing[4],
-    paddingVertical: spacing[3],
-    gap: spacing[4],
-  },
-}))
+function JobAuthor({ authorId, postAs }: { authorId: string; postAs: string }) {
+  const [authorName, setAuthorName] = useState('...')
+
+  useMount(() => {
+    if (postAs === 'company') {
+      getCompanyDetails(authorId).then((company) => {
+        if (company) setAuthorName(company.name)
+      })
+    } else {
+      setAuthorName('Invididual Employer')
+    }
+  })
+
+  return (
+    <ClText style={{ fontSize: typography.presets.base.fontSize }}>
+      {authorName}
+    </ClText>
+  )
+}
+
+function JobPostedAgo({ timestamp }: { timestamp: Date }) {
+  const [distance, setDistance] = useState('')
+
+  const computeDistance = () => {
+    return formatDistanceToNowStrict(timestamp, {
+      addSuffix: true,
+    })
+  }
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies:
+  useEffect(() => {
+    setDistance(computeDistance())
+
+    const timeout = setInterval(() => {
+      setDistance(computeDistance())
+    }, 30000)
+
+    return () => clearInterval(timeout)
+  }, [])
+
+  // Update on focus
+  useFocusEffect(() => {
+    setDistance(computeDistance())
+  })
+
+  return (
+    <ClText
+      style={{ fontSize: typography.presets.xs.fontSize }}
+      type="helper"
+      dim
+    >
+      {distance ? `Posted ${distance}` : 'Just now'}
+    </ClText>
+  )
+}
